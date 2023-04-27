df7460145c36c750bd4bbb537bd53c780373fa33
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-04-27 14:20:27 +0300

    Completed hw-04

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,9 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sum_1 = require("./sum");
 const buttonEl = document.querySelector("button");
 const num1El = document.getElementById("num1");
 const num2El = document.getElementById("num2");
-buttonEl.addEventListener("click", () => console.log((0, sum_1.sum)(+num1El.value, +num2El.value)));
+function sum(n1, n2) {
+    return n1 + n2;
+}
+buttonEl.addEventListener("click", () => console.log(sum(+num1El.value, +num2El.value)));
 //# sourceMappingURL=app.js.map
